84de528c114bb064935a084754c53869d5e42340
Erik <erikeh1@gmail.com>
2018-08-21 15:06:42 +0000

    Adds conditionals exercises

--- age-calculator.js
+++ age-calculator.js
@@ -1,6 +1,6 @@
 function ageCalculator(name, yearOfBirth, currentYear) {
     age = currentYear - yearOfBirth;
-       return name + " is " + age + " years old."
-};
+    return name + " is " + age + " years old."
+}
 
 console.log(ageCalculator('Erik', 1989, 2018));
